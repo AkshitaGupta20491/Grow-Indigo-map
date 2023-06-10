99389e67027b8e6173723abecc2dacaec250be2d
Akshita <akshianugupta@gmail.com>
2023-06-10 15:46:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+import {MapContainer,TileLayer,Marker, Popup,CircleMarker} from 'react-leaflet';
+import { Icon, divIcon, point } from "leaflet";
+import Polyline from 'react-leaflet-arrowheads'
 import './App.css';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-arrowheads';
+import L from "leaflet";
+import "./App.css";
+// import LeafletGeocoder from "./LeafletGeocoder";
+// import LeafletRoutingMachine from "./LeafletRoutingMachine";
+// import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+// import "leaflet-control-geocoder/dist/Control.Geocoder.js";
 
-function App() {
+
+
+
+function App()
+ {
+  
+  const center1=[31.1475, 75.3412];
+  const center2=[31.1571, 75.3412];
+  const redOptions = { color: 'red' };
+  const limeOptions = { color: 'lime' };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer
+    center={center1}
+    zoom={15}
+      style={{ width: '100vw', height: '100vh' }}
+    > 
+     <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <CircleMarker center={center1} pathOptions={redOptions} radius={3}>
+      <Popup>Farmer 1</Popup>
+    </CircleMarker>
+
+    <CircleMarker center={center2} pathOptions={redOptions} radius={3}>
+      <Popup>Farmer 2</Popup>
+    </CircleMarker>
+    {/* <Polyline positions={ [center1,center2] } pathOptions={limeOptions}  /> */}
+    <Polyline positions={ [center1,center2] } pathOptions={limeOptions}  arrowheads/>
+    
+
+
+
+
+    </MapContainer>
+
   );
 }
 
